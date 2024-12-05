c030237ebd722979a22b8fb0330d52f914f2bede
BenBet21 <benoit.beti@gmail.com>
2024-12-05 11:00:25 +0100

    6. Your eyes and ears on the network!"

--- linux_security/0x00_linux_security_basics/6-capture_analyze.sh
+++ linux_security/0x00_linux_security_basics/6-capture_analyze.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo tcpdump -v -c 5 // le -v sert à activer le mode verbose et -c 5 pour capturer 5 paquets
-// on peut varier le niveau de détail ave -vv ou -vvv
+sudo tcpdump -v -c 5 
+# le -v sert à activer le mode verbose et -c 5 pour capturer 5 paquets
+# on peut varier le niveau de détail ave -vv ou -vvv
