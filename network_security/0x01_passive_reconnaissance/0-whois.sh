f2e2824df41c22042124d422435a7ad27779ff95
BenBet21 <benoit.beti@gmail.com>
2024-12-10 11:20:30 +0100

    0. Who is it ?

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#!/bin/bash
 whois "$1" | awk 'BEGIN { FS=": "; OFS="," } /^(Registrant|Admin|Tech)/ { gsub(/^[ \t]+|[ \t]+$/, "", $2); if ($1 ~ /Street/) $2 = $2 " "; print $1, $2 }' > "$1.csv"
