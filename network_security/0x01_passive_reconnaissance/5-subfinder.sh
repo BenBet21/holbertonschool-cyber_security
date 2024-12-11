81835943ea6b2f8ab055c541a5832e0b57d637db
BenBet21 <benoit.beti@gmail.com>
2024-12-11 11:18:34 +0100

    5. Find the sub !

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -silent -d "$1" | while read sub; do ip=$(dig +short "$sub" | head -n 1); echo "$sub,$ip" >> "$1.txt"; echo "$sub"; done
+subfinder -silent -d "$1" -nW -o "$1.tmp" && awk -F ',' '!/,/{print $1","$2 > "'$1.txt'"} {print $1}' "$1.tmp"
