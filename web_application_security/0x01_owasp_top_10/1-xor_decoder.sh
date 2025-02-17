dcedf7cea38320502dfd0b04d905bc06dd423523
BenBet21 <benoit.beti@gmail.com>
2025-02-17 13:02:28 +0100

    1. (A2:2021) - Cryptographic Failures - Scripting

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-phrase_codee="$1"
-phrase_codee="${phrase_codee#'{xor}'}"
-phrase_decodee=$(echo -n "$phrase_codee" | base64 -d)
-sortie=""
 
-for ((n=0;n<${#phrase_decodee};n++)); do
-  char="${phrase_decodee:$n:1}"
-  printf -v xor_char "\\x$(printf '%x' "$(( $(printf '%d' "'$char") ^ 95))")"
-  sortie+="$xor_char"
+# Supprime le préfixe '{xor}' de la phrase codée
+encoded_phrase="$1"
+encoded_phrase="${encoded_phrase#'{xor}'}"
+
+# Décode la phrase en base64
+decoded_phrase=$(echo -n "$encoded_phrase" | base64 -d)
+output=""
+
+# Boucle à travers chaque caractère de la phrase décodée
+for ((i = 0; i < ${#decoded_phrase}; i++)); do
+  char="${decoded_phrase:$i:1}"
+  # Effectue l'opération XOR avec 95 et convertit en caractère
+  printf -v xor_char "\\x$(printf '%x' "$(( $(printf '%d' "'$char") ^ 95 ))")"
+  output+="$xor_char"
 done
 
-echo "$sortie"
+# Affiche la phrase décodée
+echo "$output"
