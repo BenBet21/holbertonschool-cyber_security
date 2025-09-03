b139b524c56ab2cd00529508d3402beb1c35e4a1
BenBet21 <benoit.beti@gmail.com>
2025-09-03 09:25:50 -0400

    3. Sum Attack

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Extraire les adresses IP des connexions réussies
-grep "Accepted password" auth.log | awk '{print $11}' | sort | uniq | wc -l
+grep "Accepted password for root" auth.log | awk '{print $11}' | sort | uniq | wc -l
