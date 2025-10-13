232f8444fee7871674461ac99224ccfde6029433
benbet21 <benoit.beti@gmail.com>
2025-10-13 21:37:11 +0200

    update

--- scripting_cyber/0x00-ruby_scripting/0-hello_world_function.rb
+++ scripting_cyber/0x00-ruby_scripting/0-hello_world_function.rb
@@ -1,4 +1,18 @@
 #!/usr/bin/env ruby
+# ============================================================
+# File: 0-hello_world_function.rb
+# Author: Imen (Holberton School)
+# Description: Defines a function that prints
+#              "Hello, Holberton! from <str>"
+# ============================================================
+
+# ------------------------------------------------------------
+# Function: say_hello
+# Description: Prints a greeting message from a given string.
+# Arguments:
+#   str - The string to be included in the greeting message.
+# Return: None
+# ------------------------------------------------------------
 def say_hello(str)
   puts "Hello, Holberton! from #{str}"
 end
