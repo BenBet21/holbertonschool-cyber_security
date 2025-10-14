0d63586644dc3337c1a3d493e508a8544f4fa988
Benoît BETI MARANGON <153932864+BenBet21@users.noreply.github.com>
2025-10-14 15:10:07 +0200

    Translate comments to French in write_file.rb
    Updated comments in the merge_json_files function to French.

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -3,18 +3,18 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-  # Read and parse the first JSON file
+  # Lit et convertit le 1er fichier JSON
   file1_content = File.read(file1_path)
   data1 = JSON.parse(file1_content)
 
-  # Read and parse the second JSON file
+  # Lit et convertit le 2nd fichier JSON
   file2_content = File.read(file2_path)
   data2 = JSON.parse(file2_content)
 
-  # Merge the two JSON objects (append data1 to data2)
+  # Compile les deux objets JSON (ajoute Data1 à Data2)
   merged_data = data2 + data1
 
-  # Write the merged data back to file2
+  # Ecrit la compilation effectuée dans file2
   File.write(file2_path, JSON.pretty_generate(merged_data))
 
   puts 'Merged JSON written to file.json'
