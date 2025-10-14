87c372a57488ec3068d934030829c60963889b5e
Benoît BETI MARANGON <153932864+BenBet21@users.noreply.github.com>
2025-10-14 15:04:23 +0200

    Refactor merge_json_files to accept two file paths

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/env ruby
+
 require 'json'
 
-def merge_json_files(source_path, target_path)
-  # Charger et convertir le contenu du fichier source en tableau Ruby
-  source_json_text = File.read(source_path)
-  source_entries = JSON.parse(source_json_text)
+def merge_json_files(file1_path, file2_path)
+  # Read and parse the first JSON file
+  file1_content = File.read(file1_path)
+  data1 = JSON.parse(file1_content)
 
-  # Charger et convertir le contenu du fichier cible en tableau Ruby
-  target_json_text = File.read(target_path)
-  target_entries = JSON.parse(target_json_text)
+  # Read and parse the second JSON file
+  file2_content = File.read(file2_path)
+  data2 = JSON.parse(file2_content)
 
-  # Ajouter les entrées du fichier source à celles du fichier cible
-  combined_entries = target_entries + source_entries
+  # Merge the two JSON objects (append data1 to data2)
+  merged_data = data2 + data1
 
-  # Réécrire le fichier cible avec les données fusionnées
-  File.write(target_path, JSON.pretty_generate(combined_entries))
+  # Write the merged data back to file2
+  File.write(file2_path, JSON.pretty_generate(merged_data))
 
-  # Message de confirmation (optionnel)
-  puts 'Les données JSON ont été fusionnées avec succès.'
+  puts 'Merged JSON written to file.json'
+  
 end
