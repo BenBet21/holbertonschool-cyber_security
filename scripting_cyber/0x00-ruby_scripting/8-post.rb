0b328bafc5606e17140dd1f2accc7c4330afe8ef
Benoît BETI MARANGON <153932864+BenBet21@users.noreply.github.com>
2025-10-15 14:36:19 +0200

    Refactor post_request for improved URI handling

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,24 +1,19 @@
+#!/usr/bin/env ruby
 require 'net/http'
-require 'uri'
 require 'json'
+require 'uri'
 
 def post_request(url, body_params)
   uri = URI.parse(url)
-
-  # Créer la requête POST HTTP
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = (uri.scheme == "https")
 
-  request = Net::HTTP::Post.new(uri.path, { 'Content-Type' => 'application/json' })
+  request = Net::HTTP::Post.new(uri, { 'Content-Type' => 'application/json' })
   request.body = body_params.to_json
 
-  # Envoi la reqûete et récupère la réponse
   response = http.request(request)
 
-  # Affiche le statut de la réponse et son contenu
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
-  puts JSON.generate(JSON.parse(response.body))
-rescue StandardError => e
-  puts "An error occurred: #{e.message}"
+  puts JSON.pretty_generate(JSON.parse(response.body))
 end
