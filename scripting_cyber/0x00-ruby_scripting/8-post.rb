7665812b67359b99e5eebeb9b326948eb6b10ed9
Benoît BETI MARANGON <153932864+BenBet21@users.noreply.github.com>
2025-10-15 14:28:18 +0200

    Fix URI path in HTTP POST request

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -8,7 +8,7 @@ def post_request(url, body_params)
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = (uri.scheme == "https")
 
-  request = Net::HTTP::Post.new(uri.path, { 'Content-Type' => 'application/json' })
+  request = Net::HTTP::Post.new(uri, { 'Content-Type' => 'application/json' })
   request.body = body_params.to_json
 
   response = http.request(request)
